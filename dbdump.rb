f04d0fd1e00bbc25d56e8d3130e48966dd541428
Vishnu <vishnunarang2000@gmail.com>
2017-10-02 12:13:34 -0700

    Updating db dump file

--- dbdump.rb
+++ dbdump.rb
@@ -2,6 +2,6 @@
 
 # Potential security risk if we expose this on a public server (the database type). This is only for test server.
 Dir['db/*.sqlite3'].each do |filename|
-  cmd = "sqlite3 #{filename} .dump > ~/db_dump/#{filename}.dbdump.sql"
+  cmd = "sqlite3 #{filename} .dump >> ~/db_dump/#{filename}.dbdump.sql"
   `#{cmd}`
 end
